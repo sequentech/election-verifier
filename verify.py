2511b911309705bc2570d6ad8523416f74a9f0cc
Eduardo Robles Elvira <edu@sequentech.io>
2024-03-04 08:12:55 +0100

    WIP

--- verify.py
+++ verify.py
@@ -267,7 +267,7 @@ def verify_votes_pok(pubkeys, dir_path, questions_json, search_hash):
         )
     if ballot_found:
         print_success(
-"""* Hash of the vote was successfully found:
+f"""* Hash of the vote was successfully found:
 \t- hash={hash}
 \t- voter_id={found_voter_id}
 \t- weight={weight_num}"""
